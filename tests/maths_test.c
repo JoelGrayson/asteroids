9648d9c73db67c2d98da49914bb966ed0ce3bd3d
Joel Grayson <joel@joelgrayson.com>
2026-05-25 13:03:18 -0700

    test_round passes

--- tests/maths_test.c
+++ tests/maths_test.c
@@ -6,6 +6,7 @@
 #include "../rocket.h"
 
 void test_max();
+void test_round();
 void test_sine();
 
 int main() {
@@ -13,6 +14,7 @@ int main() {
     trig_init(3);
     printf("Testing math\n");
     test_max();
+    test_round();
     test_sine();
     printf("All tests passed!\n");
 }
@@ -23,6 +25,17 @@ void test_max() {
     assert(max(-20.0, 4.0) == 4.0);
     assert(max(-20.0, 0.0) == 0.0);
     assert(max(20.0, 0.0) == 20.0);
+    printf("test_max passes\n");
+}
+
+void test_round() {
+    assert(round(1.0) == 1);
+    assert(round(-1.0) == -1);
+    assert(round(-4.4) == -4);
+    assert(round(4.5) == 5);
+    assert(round(99.9) == 100);
+    assert(round(99.3) == 99);
+    printf("test_round passes\n");
 }
 
 void test_sine() {
