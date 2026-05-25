9c632a48184f1afcd6d658e042ace417a55685ac
Joel Grayson <joel@joelgrayson.com>
2026-05-25 15:28:02 -0700

    3 types of asteroids

--- graphics/draw_asteroid.c
+++ graphics/draw_asteroid.c
@@ -1,13 +1,13 @@
 #include "draw_asteroid.h"
 
 void draw_asteroid(struct point asteroid[], color_t c) {
-    for (int i = 0; i < NUM_POINTS_PER_ASTEROID - 1; i++) {
+    for (int i = 0; i < ASTEROID_A_NUM_POINTS - 1; i++) {
         draw_line(asteroid[i], asteroid[i + 1], c);
     }
 }
 
 
-struct point ASTEROID_1_POINTS[NUM_POINTS_PER_ASTEROID] = {
+struct point ASTEROID_A_SMALL[ASTEROID_A_NUM_POINTS] = {
     { 10, 5 },
     { 19, 5 },
     { 27, 9 },
@@ -21,5 +21,34 @@ struct point ASTEROID_1_POINTS[NUM_POINTS_PER_ASTEROID] = {
     { 4, 9 },
     { 13, 9 },
     { 10, 5 }
+};
+
+struct point ASTEROID_B_SMALL[ASTEROID_B_NUM_POINTS] = {
+    { 10, 5 },
+    { 15, 10 },
+    { 21, 5 },
+    { 26, 10 },
+    { 23, 15 },
+    { 26, 20 },
+    { 18, 25 },
+    { 10, 25 },
+    { 4, 20 },
+    { 4, 17 },
+    { 4, 10 },
+};
+
+struct point ASTEROID_C_SMALL[ASTEROID_C_NUM_POINTS] = {
+    { 10, 5 },
+    { 4, 10 },
+    { 7, 15 },
+    { 4, 19 },
+    { 9, 24 },
+    { 13, 21 },
+    { 21, 24 },
+    { 26, 17 },
+    { 21, 13 },
+    { 26, 9 },
+    { 21, 5 },
+    { 15, 7 },
 };
 
--- graphics/draw_asteroid.h
+++ graphics/draw_asteroid.h
@@ -6,7 +6,11 @@
 
 void draw_asteroid(struct point asteroid[], color_t c);
 
-#define NUM_POINTS_PER_ASTEROID 13
-extern struct point ASTEROID_1_POINTS[NUM_POINTS_PER_ASTEROID]; //extern means not a declaration but rather telling compiler that it is there somewhere and linker will find it
+#define ASTEROID_A_NUM_POINTS 13
+#define ASTEROID_B_NUM_POINTS 11
+#define ASTEROID_C_NUM_POINTS 12
+extern struct point ASTEROID_A_SMALL[ASTEROID_A_NUM_POINTS]; //extern means not a declaration but rather telling compiler that it is there somewhere and linker will find it
+extern struct point ASTEROID_B_SMALL[ASTEROID_B_NUM_POINTS]; //extern means not a declaration but rather telling compiler that it is there somewhere and linker will find it
+extern struct point ASTEROID_C_SMALL[ASTEROID_C_NUM_POINTS]; //extern means not a declaration but rather telling compiler that it is there somewhere and linker will find it
 
 #endif
