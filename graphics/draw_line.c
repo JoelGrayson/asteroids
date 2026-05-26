155528040c0ab65968a63c2bd2078583f8367609
Joel Grayson <joel@joelgrayson.com>
2026-05-25 17:11:48 -0700

    switch from draw_line being point based to instead double-based for easy manipulation of points

--- graphics/draw_line.c
+++ graphics/draw_line.c
@@ -1,34 +1,36 @@
 #include "draw_line.h"
 
-void draw_line(struct point p1, struct point p2, color_t c) {
+// (x1, y1) is point 1, (x2, y2) is point 2
+void draw_line(double x1, double y1, double x2, double y2, color_t c) {
     // dx/dy is what you have to do to move from p1 to p2
-    double dx = p2.x - p1.x;
-    double dy = p2.y - p1.y;
+    double dx = x2 - x1;
+    double dy = y2 - y1;
 
-    // If y is the larger difference, move from p1.y to p2.y by ±1 and along the way have an appropriate x increase (by ±<1)
+    // If y is the larger difference, move from y1 to y2 by ±1 and along the way have an appropriate x increase (by ±<1)
     axis direction_of_most_movement = abs(dy) > abs(dx) ? Y : X;
 
     // pos moves from point 1 to point 2
-    struct point pos = p1;
+    double posx = x1;
+    double posy = y1;
     
     if (direction_of_most_movement == Y) {
         int sgn_dy = sgn(dy);
-        // For every +1 in pos.y, pos.x increases by dx/dy
+        // For every +1 in posy, posx increases by dx/dy
         double dx_div_dy = dx / dy;
 
         while (true) {
-            gl_draw_pixel(round(pos.x), round(pos.y), c); //double rounded to int so it draws the nearest pixel
+            gl_draw_pixel(round(posx), round(posy), c); //double rounded to int so it draws the nearest pixel
 
-            pos.y += sgn_dy; //change by ±1
-            pos.x += sgn_dy * dx_div_dy;
+            posy += sgn_dy; //change by ±1
+            posx += sgn_dy * dx_div_dy;
 
             // Check if done (drew past p2). If done, return
             if (
                 // Increasing
-                (sgn_dy == 1 && pos.y >= p2.y)
+                (sgn_dy == 1 && posy >= y2)
                 ||
                 // Decreasing
-                (sgn_dy == -1 && pos.y <= p2.y)
+                (sgn_dy == -1 && posy <= y2)
             ) {
                 return;
             }
@@ -38,17 +40,17 @@ void draw_line(struct point p1, struct point p2, color_t c) {
         double dy_div_dx = dy / dx;
 
         while (true) {
-            gl_draw_pixel(round(pos.x), round(pos.y), c);
+            gl_draw_pixel(round(posx), round(posy), c);
             
-            pos.x += sgn_dx;
-            pos.y += sgn_dx * dy_div_dx;
+            posx += sgn_dx;
+            posy += sgn_dx * dy_div_dx;
 
             if (
                 // Moving right
-                (sgn_dx == 1 && pos.x >= p2.x)
+                (sgn_dx == 1 && posx >= x2)
                 ||
                 // Moving left
-                (sgn_dx == -1 && pos.x <= p2.x)
+                (sgn_dx == -1 && posx <= x2)
             ) {
                 return;
             }
