c5750b05f574b1183f2a7a5c04313166a16c79bb
Joel Grayson <joel@joelgrayson.com>
2026-05-26 20:20:24 -0700

    rocket vector in rocket.c

--- rocket.c
+++ rocket.c
@@ -2,6 +2,15 @@
 #include "mechanics.h"
 #include "constants.h"
 
+struct point ROCKET[ROCKET_NUM_POINTS] = {
+    { 25, 5 },
+    { 12, 45 },
+    { 15, 36 },
+    { 35, 36 },
+    { 38, 45 }
+};
+
+
 static struct mechanics rocket_mechanics = {
     .x = MONITOR_WIDTH / 2,
     .y = MONITOR_HEIGHT / 2,
--- rocket.h
+++ rocket.h
@@ -1,5 +1,10 @@
 #ifndef ROCKET_H
 #define ROCKET_H
 
+#define ROCKET_NUM_POINTS 5
+
+#include "graphics/point.h"
+
+extern struct point ROCKET[ROCKET_NUM_POINTS];
 
 #endif
