c17e858b20332bc31376e30003c3d1df47493f68
ZopesPrograms <zopesconk1@gmail.com>
2026-05-31 22:16:14 -0700

    Line segments intersect detector function finished

--- graphics/geometry.c
+++ graphics/geometry.c
@@ -35,3 +35,18 @@ struct vector vec_orthogonal(struct vector vec) {
     };
     return ortho;
 }
+
+bool lines_intersect(struct point p1, struct point p2, struct point p3, struct point p4) {
+    double t_num = (p1.x - p3.x)*(p3.y - p4.y) - (p1.y - p3.y)*(p3.x - p4.x);
+    double t_den = (p1.x - p2.x)*(p3.y - p4.y) - (p1.y - p2.y)*(p3.x - p4.x);
+
+    double u_num = (p1.x - p3.x)*(p1.y - p2.y) - (p1.x - p2.x)*(p1.y - p3.y);
+    double u_den = (p1.x - p2.x)*(p3.y - p4.y) - (p3.x - p4.x)*(p1.y - p2.y);
+
+    if(!(t_num >= 0 && t_den > 0) && !(t_num <= 0 && t_den < 0)) return false;
+    if(!(u_num >= 0 && u_den > 0) && !(u_num <= 0 && u_den < 0)) return false;
+
+    if(abs(t_num/t_den) > 1) return false;
+    if(abs(u_num/u_den) > 1) return false;
+    return true;
+}
--- graphics/geometry.h
+++ graphics/geometry.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "point.h"
+#include <stdbool.h>
 
 struct vector {
     double x;
@@ -13,3 +14,6 @@ struct point point_plus_vec(struct point fulcrum, struct vector dir); // returns
 struct vector vec_difference(struct point p1, struct point p2); // takes vector defining difference of two points
 struct vector vec_normalize(struct vector vec);  // returns normalized vector
 struct vector vec_orthogonal(struct vector vec); // returns orthogonal vector
+
+// Returns truth value of whether line segments (p1, p2) and (p3, p4) intersect or not
+bool lines_intersect(struct point p1, struct point p2, struct point p3, struct point p4);
