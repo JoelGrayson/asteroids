88da7fc23ee0aa9672ebf4672c38f73499d81687
Joel Grayson <joel@joelgrayson.com>
2026-06-03 13:11:02 -0700

    center saucer points, SMALL and BIG saucer variants

--- saucer.c
+++ saucer.c
@@ -1,27 +1,72 @@
 #include "saucer.h"
 
-struct point SAUCER_EXTERIOR_POINTS[] = {
-    { 25, 16 },
-    { 35, 16 },
-    { 39, 25 },
-    { 52, 35 },
-    { 41, 45 },
-    { 21, 45 },
-    { 10, 35 },
-    { 21, 25 },
-    { 25, 16 }
+#define NUM_SAUCER_EXTERIOR_POINTS (sizeof(BIG_SAUCER_EXTERIOR_POINTS) / sizeof(BIG_SAUCER_EXTERIOR_POINTS[0]))
+struct point BIG_SAUCER_EXTERIOR_POINTS[] = {
+    { -6, -17 },
+    { 4, -17 },
+    { 8, -8 },
+    { 21, 2 },
+    { 10, 12 },
+    { -10, 12 },
+    { -21, 2 },
+    { -10, -8 },
+    { -6, -17 }
 };
-#define NUM_SAUCER_EXTERIOR_POINTS (sizeof(SAUCER_EXTERIOR_POINTS) / sizeof(SAUCER_EXTERIOR_POINTS[0]))
-struct point SAUCER_TOP_LINE_POINTS[] = {
-    { 21, 25 },
-    { 39, 25 }
+struct point BIG_SAUCER_TOP_LINE_POINTS[] = {
+    { -10, -8 },
+    { 8, -8 }
 };
-struct point SAUCER_BOTTOM_LINE_POINTS[] = {
-    { 10, 35 },
-    { 52, 35 }
+struct point BIG_SAUCER_BOTTOM_LINE_POINTS[] = {
+    { -21, 2 },
+    { 21, 2 }
 };
 
+#define SMALL_SAUCER_RATIO 0.7
+struct point SMALL_SAUCER_EXTERIOR_POINTS[] = {
+    { -6  * SMALL_SAUCER_RATIO, -17 * SMALL_SAUCER_RATIO },
+    { 4   * SMALL_SAUCER_RATIO, -17 * SMALL_SAUCER_RATIO },
+    { 8   * SMALL_SAUCER_RATIO, -8  * SMALL_SAUCER_RATIO },
+    { 21  * SMALL_SAUCER_RATIO, 2   * SMALL_SAUCER_RATIO },
+    { 10  * SMALL_SAUCER_RATIO, 12  * SMALL_SAUCER_RATIO },
+    { -10 * SMALL_SAUCER_RATIO, 12  * SMALL_SAUCER_RATIO },
+    { -21 * SMALL_SAUCER_RATIO, 2   * SMALL_SAUCER_RATIO },
+    { -10 * SMALL_SAUCER_RATIO, -8  * SMALL_SAUCER_RATIO },
+    { -6  * SMALL_SAUCER_RATIO, -17 * SMALL_SAUCER_RATIO }
+};
+struct point SMALL_SAUCER_TOP_LINE_POINTS[] = {
+    { -10 * SMALL_SAUCER_RATIO, -8  * SMALL_SAUCER_RATIO },
+    { 8   * SMALL_SAUCER_RATIO, -8 *  SMALL_SAUCER_RATIO }
+};
+struct point SMALL_SAUCER_BOTTOM_LINE_POINTS[] = {
+    { -21 * SMALL_SAUCER_RATIO, 2  *  SMALL_SAUCER_RATIO },
+    { 21  * SMALL_SAUCER_RATIO, 2 *   SMALL_SAUCER_RATIO }
+};
+
+static struct mechanics saucer_mechanics;
+static enum saucer_state saucer_state;
+
 void render_saucer() {
-    draw_points(SAUCER_EXTERIOR_POINTS, NUM_SAUCER_EXTERIOR_POINTS, 100, 100, GL_WHITE);
+    if (saucer_state == NO_SAUCER) {
+
+    } else if (saucer_state == BIG_SAUCER) {
+        draw_points(BIG_SAUCER_EXTERIOR_POINTS, NUM_SAUCER_EXTERIOR_POINTS, saucer_mechanics.x, saucer_mechanics.y, GL_WHITE);
+        draw_points(BIG_SAUCER_TOP_LINE_POINTS, NUM_SAUCER_EXTERIOR_POINTS, saucer_mechanics.x, saucer_mechanics.y, GL_WHITE);
+        draw_points(BIG_SAUCER_BOTTOM_LINE_POINTS, NUM_SAUCER_EXTERIOR_POINTS, saucer_mechanics.x, saucer_mechanics.y, GL_WHITE);
+    } else if (saucer_state == SMALL_SAUCER) {
+        draw_points(SMALL_SAUCER_EXTERIOR_POINTS, NUM_SAUCER_EXTERIOR_POINTS, saucer_mechanics.x, saucer_mechanics.y, GL_WHITE);
+        draw_points(SMALL_SAUCER_TOP_LINE_POINTS, NUM_SAUCER_EXTERIOR_POINTS, saucer_mechanics.x, saucer_mechanics.y, GL_WHITE);
+        draw_points(SMALL_SAUCER_BOTTOM_LINE_POINTS, NUM_SAUCER_EXTERIOR_POINTS, saucer_mechanics.x, saucer_mechanics.y, GL_WHITE);
+    } else {
+        printf("Error: invalid saucer state %s\n", saucer_state);
+    }
+}
+
+
+int frames_until_saucer_respawns = FPS * 10; //takes 10 seconds for a new saucer to spawn
+static int frame_when_saucer_last_despawned = 0; //when saucer dies, set this to the frame number. If 
+
+
+void saucer_frame_call(frame_number) {
+
 }
 
--- saucer.h
+++ saucer.h
@@ -1,5 +1,16 @@
 #include "graphics/point.h"
 #include "graphics/draw_points.h"
+#include "fps.h"
+#include "mechanics.h"
+#include <stdbool.h>
+#include "printf.h"
+
+enum saucer_state {
+    NO_SAUCER, //no saucer on screen
+    SMALL_SAUCER, //the tiny saucer (harder) which shoots more accurately
+    BIG_SAUCER //the clumsy big saucer
+};
 
 void render_saucer();
+void saucer_frame_call(frame_number); //renders the saucer and decides whether to spawn another saucer
 
