6a9ddf48d8c1fd3bf892f863e61a99ab98e65dbd
Joel Grayson <joel@joelgrayson.com>
2026-06-03 19:52:34 -0700

    proper rotation matrix

--- AI_USAGE.md
+++ AI_USAGE.md
@@ -1,4 +1,5 @@
 # AI Usage
+* Rotation matrix equation
 * Center-align my rocket and explosion points
 * Learned that NULL can be assigned to any pointer type and `= { 0 }` is how you set a new array to all NULL (instead of garbage value because not zeroed out by default). Also if (x == NULL) can be written as if (!x) because of the falsiness of NULL
 * Used Claude cowork to convert the .wav audio files from 8 bit to 16 bit and 11 kHz to 48 kHz
--- graphics/rotate_points.c
+++ graphics/rotate_points.c
@@ -13,7 +13,7 @@ void rotate_points(struct point *points, int num_points, double theta) {
 void rotate_template_points(struct point *points_dst, struct point *points_template, int num_points, double theta) {
     for(int i = 0; i < num_points; i++) {
         struct point to_rotate = points_template[i];
-        points_dst[i].x = (cosine(theta) * to_rotate.x) + (sine(theta) * to_rotate.y);
-        points_dst[i].y = (-1*sine(theta) * to_rotate.x) + (cosine(theta) * to_rotate.y);
+        points_dst[i].x = cosine(theta) * to_rotate.x - sine(theta) * to_rotate.y;
+        points_dst[i].y = sine(theta) * to_rotate.x + cosine(theta) * to_rotate.y;
     }
 }
