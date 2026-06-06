5b65698519070b705aa7324512091e0c7ffbee7d
Joel Grayson <joel@joelgrayson.com>
2026-06-06 16:09:55 -0700

    use LLM-generated atan2 instead

--- atan2.h
+++ atan2.h
@@ -2,6 +2,6 @@
 
 #include "maths.h"
 
-// Copied from https://github.com/insidegui/flux/blob/master/libc/math/atan2.c
+// Self-contained atan2 (LUT-based). Works out of the box with no init call.
+// Returns the angle in radians in the range (-PI, PI].
 double atan2(double y, double x);
-
